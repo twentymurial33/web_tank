7c9fc1322c57110c0d1dd0d1793e366677af4b11
Murial Anindo <murial.anindo@lonett.com>
2022-04-27 12:51:04 -0400

    added signon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Main from "./Main";
 import About from "./About";
 import { BrowserRouter as Router, useRoutes } from "react-router-dom";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
 const AppWrapper = () => {
   let routes = useRoutes([
@@ -19,9 +21,42 @@ const AppWrapper = () => {
 function App() {
   return (
     <div className="App">
-      <Router>
-        <AppWrapper />
-      </Router>
+      <Authenticator>
+        {({ signOut, user }) => (
+          <div style={styles.container}>
+            <h1>Hello {user.username}</h1>
+            <button style={styles.button} onClick={signOut}>
+              Sign out
+            </button>
+            <br />
+            <h2>Amplify Todos</h2>
+            <input
+              onChange={(event) => setInput("name", event.target.value)}
+              style={styles.input}
+              value={formState.name}
+              placeholder="Name"
+            />
+            <input
+              onChange={(event) => setInput("description", event.target.value)}
+              style={styles.input}
+              value={formState.description}
+              placeholder="Description"
+            />
+            <button style={styles.button} onClick={addTodo}>
+              Create Todo
+            </button>
+            {todos.map((todo, index) => (
+              <div key={todo.id ? todo.id : index} style={styles.todo}>
+                <p style={styles.todoName}>{todo.name}</p>
+                <p style={styles.todoDescription}>{todo.description}</p>
+              </div>
+            ))}
+          </div>
+        )}
+        <Router>
+          <AppWrapper />
+        </Router>
+      </Authenticator>
     </div>
   );
 }
