aa3b4186745b650114ba6e4be63558660c7d84d4
Murial Anindo <murial.anindo@lonett.com>
2022-04-26 20:33:02 -0400

    added

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -9,6 +9,7 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
+import { Link } from "react-router-dom";
 
 export default function FixedDrawer() {
   const [state, setState] = React.useState({
