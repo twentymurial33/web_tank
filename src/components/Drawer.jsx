348c6624005b0b5a1735ad726422efafdb25941c
Murial Anindo <murial.anindo@lonett.com>
2022-05-04 10:13:06 -0400

    removed divider import

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -3,7 +3,6 @@ import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
 import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
@@ -45,17 +44,6 @@ export default function FixedDrawer() {
           </ListItem>
         ))}
       </List>
-      <Divider />
-      {/* <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List> */}
     </Box>
   );
 
